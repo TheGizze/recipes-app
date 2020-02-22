e87be51d66b1adc3988c014894315a250a63120f
Teemu Kurki <teemukurki4@gmail.com>
2020-02-22 17:57:33 +0200

    feat: added frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	// webpack will take the files from ./src/index
+	entry: "./src/index",
+
+	// and output it into /dist as bundle.js
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader"
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./static/index.html",
+			filename: "./index.html"
+		})
+	]
+};
