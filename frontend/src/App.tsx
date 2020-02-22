179f3b217d98cc6ba2f833d9fd5a909ef33556fa
Teemu Kurki <teemukurki4@gmail.com>
2020-02-22 18:34:35 +0200

    feat: added lint and prettier configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
-	return (
-		<div className="container">
-			<h1>Hello world</h1>
-		</div>
-	);
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="container">
+      <h1>Hello world</h1>
+    </div>
+  );
 };
 
 export default App;
