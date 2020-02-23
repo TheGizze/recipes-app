1bf5f158a5ee86110c011346a64f441b41c44846
Miika Halmela <miika.halmela@gmail.com>
2020-02-23 15:51:15 +0200

    defined functions for recipes controller

--- recipes-api/recipes_api/web/controllers/recipe_controller.py
+++ recipes-api/recipes_api/web/controllers/recipe_controller.py
@@ -3,6 +3,15 @@ recipes ={"item": "test"}
 def get_recipe():
     return recipes["item"], 200
 
-def remove_recipe():
+def delete_recipe():
     recipes.clear()
+    return 200
+
+def put_recipe():
+    return 200
+
+def post_recipe():
+    return 200
+
+def get_recipes():
     return 200
