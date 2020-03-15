fa6ee41304a01f242c1a12624855460cf506e866
Miika Halmela <miika.halmela@gmail.com>
2020-03-15 16:59:36 +0200

    updated recepie model

--- recipes-api/recipes_api/web/controllers/recipe_controller.py
+++ recipes-api/recipes_api/web/controllers/recipe_controller.py
@@ -1,17 +1,33 @@
-recipes ={"item": "test"}
+recipes = {
+          "1": {"name": "template_name",
+          "description": "some neat description of the dish",
+          "ingredients": [{"ingredient_name": "tomato", "measurement": "1"}]
+          "steps": ["step 1", "step 2", "step 3", "step 4"],
+          "tags": ["tag 1", "tag 2"]}
+}
 
-def get_recipe():
-    return recipes["item"], 200
+next_id = 2
 
-def delete_recipe():
-    recipes.clear()
+
+def get_recipe(id):
+    return recipes[id], 200
+
+
+def delete_recipe(id):
+    recipes.pop(id)
     return 200
 
-def put_recipe():
+
+def put_recipe(id, req_body):
+    recipes.update(id, req_body)
     return 200
 
-def post_recipe():
+
+def post_recipe(req_body):
+    recipes[str(next_id)] = req_body
+    next_id += 1
     return 200
+
 
 def get_recipes():
-    return 200
+    return recipes, 200
